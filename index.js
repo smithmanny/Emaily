c80fa615159791a0d36a1bf0793f40da2728789b
Shakhor Smith <shakhorsmith@gmail.com>
2017-09-09 19:47:04 -0400

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ name: 'Shakhor' });
 });
 
 app.listen(PORT);

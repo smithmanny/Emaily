85f9e9b2bf83c4d6b22f4ce47ae58484f765e9e9
Shakhor Smith <shakhorsmith@gmail.com>
2017-09-10 16:17:28 -0400

    finished oauth

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+// Require JS Files
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+app.use(
+  cookieSession({
+    // Cookie last for 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send({ name: 'Shakhor' });
+app.use(passport.initialize());
+app.use(passport.session());
+
+// get OAuth routes
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
-
-app.listen(PORT);
